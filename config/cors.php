9bd0d7dbc23cd867c5edcea6f5dae69738225e65
中村　迅 <shu.kyu0224@icloud.com>
2021-08-22 15:28:24 +0900

    rese-laravel

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,20 @@
 <?php
 
 return [
-    'paths' => ['*'],
+    // 'paths' => ['*'],
+    // 'allowed_methods' => ['*'],
+    // 'allowed_origins' => ['*'],
+    // 'allowed_origins_patterns' => [],
+    // 'allowed_headers' => ['*'],
+    // 'exposed_headers' => false,
+    // 'max_age' => false,
+    // 'supports_credentials' => false,
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => false,
-    'max_age' => false,
+    'exposed_headers' => [],
+    'max_age' => 0,
     'supports_credentials' => false,
 ];
