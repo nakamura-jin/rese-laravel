838d16f55fd97bced3e0dc666eaa5fcf812f37fc
中村　迅 <shu.kyu0224@icloud.com>
2021-08-27 01:20:42 +0900

    rese-laravel

--- config/logging.php
+++ config/logging.php
@@ -38,13 +38,11 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
         ],
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'debug',
         ],
 
         'daily' => [
